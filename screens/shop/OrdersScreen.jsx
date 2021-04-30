65b62e6b605db0e09755da38c750c4a46176f20e
EvgenyMinin <evgeny.frontend@gmail.com>
2021-04-30 21:20:22 +0300

    added order items

--- screens/shop/OrdersScreen.jsx
+++ screens/shop/OrdersScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
+import OrderItem from '../../components/shop/OrderItem';
 
 const OrdersScreen = () => {
   const orders = useSelector((state) => state.orders.orders);
@@ -9,7 +10,9 @@ const OrdersScreen = () => {
     <FlatList
       data={orders}
       keyExtractor={({ id }) => id}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={({ item }) => (
+        <OrderItem amount={item.totalAmount} date={item.readableDate} />
+      )}
     />
   );
 };
